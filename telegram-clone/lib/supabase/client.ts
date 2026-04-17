a4ea19794e5176234553292f109f4a3a5d92b463
aure1337 <danituet@gmail.com>
2026-04-18 01:29:54 +0300

    Delete telegram-clone directory

--- telegram-clone/lib/supabase/client.ts
+++
@@ -1,4 +0,0 @@
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { Database } from '@/types/database.types';
-
-export const createClient = () => createClientComponentClient<Database>();
